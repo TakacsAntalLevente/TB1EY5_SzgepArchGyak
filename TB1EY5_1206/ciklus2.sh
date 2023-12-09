194339265c8c58a089a72851230da25ea800904a
TakacsAntalLevente <146961682+TakacsAntalLevente@users.noreply.github.com>
2023-12-09 19:41:06 +0100

    Update ciklus2.sh

--- TB1EY5_1206/ciklus2.sh
+++ TB1EY5_1206/ciklus2.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
 
+declare -ri n=$RANDOM
 
-echo $((RANDOM % 50 +1))
+echo "A random szán: $n"
 
+declare -i i=0
+declare -i square_i=0
+while [ $square_i -lt $n ]; do
+	echo $square_i
+	i=$((i + 1))
+	square_i=$((i * i))
+done
