94ca787c2a15eaf936228e7e1e3dc29e7a1582aa
TakacsAntalLevente <146961682+TakacsAntalLevente@users.noreply.github.com>
2023-12-09 19:42:16 +0100

    Update ciklus1.sh

--- TB1EY5_1206/ciklus1.sh
+++ TB1EY5_1206/ciklus1.sh
@@ -1,5 +1,23 @@
+echo 'For:'
+
 for i in {1..10}
 do
 echo $i
 i=i-1
+done
+
+echo 'while:'
+
+declare -i i=0
+while [ $i -le 10 ]; do
+	echo $i
+	i=$((i + 1))
+done
+
+echo 'until:'
+
+declare -i i=0
+until [ $i -gt 10 ]; do
+	echo $i
+	i=$((i + 1))
 done
